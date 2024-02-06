4feaf8e6cfac943f23da31bf539efa916b7d9bed
Gal Weiss <weiss_gal@yahoo.com>
2024-02-06 04:03:43 +0200

    pylint fixes

--- cb_bot/TaskException.py
+++ cb_bot/TaskException.py
@@ -1,8 +1,11 @@
 class TaskException(Exception):
+    """
+    Exception that indicates that a task has failed
+    """
     def __init__(self, message):
         super().__init__(message)
         self.message = message
-        
+
     def __str__(self):
         return self.message
 
@@ -10,6 +13,3 @@ class TaskFatalException(TaskException):
     """
     Exception that indicates that a task has failed so fatally that the bot should stop
     """
-    def __init__(self, message):
-        super().__init__(message)
-        
